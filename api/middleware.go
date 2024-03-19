2e304e66df4970c16601dd259b657ec7663675b1
ArtemSmotritel <artemsmotritel@gmail.com>
2024-03-19 22:37:17 +0200

    feature implement middleware  - redirectUserMiddleware to redirect to and fro login page  - protectAuctionsMiddleware to forbid not an auction owner to edit the auction

--- api/middleware.go
+++ api/middleware.go
@@ -3,10 +3,81 @@ package api
 import (
 	"context"
 	"fmt"
+	"github.com/artemsmotritel/oktion/templates"
 	"github.com/artemsmotritel/oktion/utils"
 	"log"
 	"net/http"
+	"slices"
+	"strconv"
 )
+
+func (s *Server) protectAuctionsMiddleware(next http.HandlerFunc, auctionIdWildcard string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		isAuth, err := utils.ExtractValueFromContext[bool](r.Context(), "isAuthorized")
+		if err != nil {
+			isAuth = false
+		}
+
+		if !isAuth {
+			w.WriteHeader(http.StatusUnauthorized)
+			handler := templates.NewErrorPageHandler(templates.Unauthorized)
+			handler.ServeHTTP(w, r)
+			return
+		}
+
+		userId, err := utils.ExtractValueFromContext[int64](r.Context(), "userId")
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			handler := templates.NewErrorPageHandler(templates.Unauthorized)
+			handler.ServeHTTP(w, r)
+			return
+		}
+
+		auctionId, err := strconv.ParseInt(r.PathValue(auctionIdWildcard), 10, 64)
+		if err != nil {
+			s.badRequestError(w, r, fmt.Sprintf("Bad auction id in path: %s", r.PathValue("id")))
+			return
+		}
+
+		actualOwnerId, err := s.store.GetOwnerIDByAuctionID(auctionId)
+
+		if userId != actualOwnerId {
+			w.WriteHeader(http.StatusForbidden)
+			handler := templates.NewErrorPageHandler(templates.Forbidden)
+			handler.ServeHTTP(w, r)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	}
+}
+
+func redirectUserMiddleware(next http.Handler) http.Handler {
+	pathsToRedirectAuthorizedUser := []string{"/login", "/redirect-me"}
+	pathsToRedirectUnauthorizedUser := []string{"/auctions/new", "/redirect-me"}
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		isAuth, err := utils.ExtractValueFromContext[bool](r.Context(), "isAuthorized")
+		if err != nil {
+			isAuth = false
+		}
+
+		if isAuth && slices.Contains[[]string](pathsToRedirectAuthorizedUser, r.URL.Path) {
+			w.Header().Set("Location", "/profile")
+			w.WriteHeader(http.StatusSeeOther)
+			return
+		}
+
+		if !isAuth && slices.Contains[[]string](pathsToRedirectUnauthorizedUser, r.URL.Path) {
+			w.Header().Add("HX-Location", "/login")
+
+			w.WriteHeader(http.StatusSeeOther)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
 
 func setUserInfoToContextMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -19,7 +90,9 @@ func setUserInfoToContextMiddleware(next http.Handler) http.Handler {
 			r = r.WithContext(context.WithValue(r.Context(), "isAuthorized", true))
 		}
 
-		r = r.WithContext(context.WithValue(r.Context(), "hxBoosted", r.Header.Get("HX-Boosted") == "true"))
+		// TODO choose an appropriate name for the context value
+		// that should mark that the response should return only a part of the html and not the whole page
+		r = r.WithContext(context.WithValue(r.Context(), "hxBoosted", r.Header.Get("HX-Request") == "true"))
 
 		next.ServeHTTP(w, r)
 	})
