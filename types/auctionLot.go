f3f0237493cf78e91a63d23d4cd56af067a480fa
Artem Smotrytel <artemsmotritel@gmail.com>
2024-05-03 02:10:58 +0300

    feat expand the AuctionLot type

--- types/auctionLot.go
+++ types/auctionLot.go
@@ -1,10 +1,23 @@
 package types
 
+import (
+	"database/sql"
+	decimal "github.com/jackc/pgx-shopspring-decimal"
+	"time"
+)
+
 type AuctionLot struct {
-	ID          int64
-	AuctionID   int64
-	Name        string
-	Description string
+	ID           int64
+	AuctionID    int64
+	Name         string
+	Description  string
+	IsActive     bool
+	MinimalBid   decimal.Decimal
+	ReservePrice decimal.Decimal
+	BinPrice     decimal.Decimal
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	DeletedAt    sql.NullTime
 }
 
 func CopyAuctionLot(auctionLot *AuctionLot) *AuctionLot {
